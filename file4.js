00e7213e558daec7714aec17153a5a49d5ca16f5
ansh-stack00 <ansha.hestabit@gmail.com>
2025-11-05 17:10:31 +0530

    Revert "Fixed : buggy code"
    This reverts commit f4d1eea2277eb2dfc028f26ffb2ff5c474d80855.

--- file4.js
+++ file4.js
@@ -1,3 +1,1 @@
-console.log('File 4: Buggy file'); 
-let result = 10 / 2; 
-console.log('Result:', result.toFixed(2));
+console.log('File 4: Buggy file'); let result = 10 / 0; console.log('Result:', result.toFixed(2));
