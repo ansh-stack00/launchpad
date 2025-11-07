a8a6ef4d696e3bef34e37fcb839b4b1f24519dc4
ansh-stack00 <ansha.hestabit@gmail.com>
2025-11-07 13:40:26 +0530

    Created : script to validate which ensure src and config.json exist.

---
+++ validate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+LOG_FILE_PATH="logs/validate.log"
+
+# if dir not present make it 
+mkdir -p logs
+
+echo "validation run at $(date) " >> "$LOG_FILE_PATH"
+
+
+# checking src folder exist or not 
+
+if [ ! -d "src" ]; then
+  echo "[ERROR] src/ directory missing!" | tee -a "$LOG_FILE_PATH"
+  exit 1
+else
+  echo "[INFO] src/ directory found and not empty." | tee -a "$LOG_FILE_PATH"
+fi
+
+
+# checking config.json exist or not 
+if ! jq empty config.json 2>/dev/null; then
+  echo "[ERROR] config.json is invalid JSON!" | tee -a "$LOG_FILE_PATH"
+  exit 1
+else
+  echo "[INFO] config.json found and not empty." | tee -a "$LOG_FILE_PATH"
+fi
+
+echo "[OK] All checks passed!" | tee -a "$LOG_FILE_PATH"
+exit 0
