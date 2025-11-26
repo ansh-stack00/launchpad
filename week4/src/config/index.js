598187b42738759998ec2b2838c302b54a99fed0
ansh-stack00 <ansha.hestabit@gmail.com>
2025-11-26 12:12:22 +0530

    conig the dotenv

---
+++ week4/src/config/index.js
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+export default function loadConfig() {
+
+    const env = process.env.NODE_ENV || "local"
+
+    const envFile = {
+        local : ".env.local",
+        dev : ".env.dev",
+        prod : ".env.prod"
+    }[env] || ".env.local"
+
+    // resolving path for dotenv
+    const resolvedPath = path.resolve(process.cwd , envFile); 
+
+    dotenv.config({
+        path:resolvedPath
+    })
+
+    return {
+        env ,
+        port : process.env.PORT ? Number(process.env.PORT) : 8080,
+        dbURI: process.env.DATABASE_URI || ""
+    }
+}
+
