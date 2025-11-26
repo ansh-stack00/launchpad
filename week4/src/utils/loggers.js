8d54ee229ce9f44be3e3e3e111279a07aff4059c
ansh-stack00 <ansha.hestabit@gmail.com>
2025-11-26 12:23:01 +0530

    added logger utility

---
+++ week4/src/utils/loggers.js
@@ -0,0 +1,29 @@
+import winston from "winston";
+import fs from "fs";
+
+
+// check if logs dir exist or not 
+//  resolved to src/logs
+const logDir = path.join(process.cwd , "src", "logs")
+
+if(!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir , { recursive:true })
+}
+
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [${level.toUpperCase()}] ${message}`;
+    })
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: path.join(logDir, "app.log") })
+  ],
+});
+
+
+export default logger ;
