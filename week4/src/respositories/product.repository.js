b3e2130990ddd49a1120828c2202b317dfb96e35
ansh-stack00 <ansha.hestabit@gmail.com>
2025-11-28 12:34:37 +0530

    refactor : add build query method in product repository

--- week4/src/respositories/product.repository.js
+++ week4/src/respositories/product.repository.js
@@ -1,43 +1,71 @@
-import Product from "../models/Product.model.js";
+import Product from "../models/Product.model.js"
 
 class ProductRepository {
-  
+
   async create(data) {
-    return await Product.create(data);
+    const product = new Product(data);
+    return await product.save();
   }
 
-  async findById(id) {
-    return await Product.findById(id);
+  // exclude soft deleted products 
+  async findById (id) {
+    return await Product.findById(id).where('deletedAt').equals(null);
   }
 
-  async findPaginated({ page = 1, limit = 10 }) {
-    const skip = (page - 1) * limit;
 
-    const products = await Product.find()
-      .sort({ createdAt: -1 })
-      .skip(skip)
-      .limit(limit);
+  async findPaginated(filters , page=1 , limit = 10) {
+    const query = this.buildQuery(filters);
 
-    const count = await Product.countDocuments();
+    const product = await Product.find(query)
+                    .skip((page-1) * limit)
+                    .limit(limit)
+                    .sort({createdAt : -1})
 
-    return {
-      page,
-      totalPages: Math.ceil(count / limit),
-      totalItems: count,
-      data: products,
-    };
+    return product;
   }
 
-  async update(id, data) {
-    return await Product.findByIdAndUpdate(id, data, {
-      new: true,
-      runValidators: true,
-    });
+  async update (id , data ) {
+
+    return await Product.findByIdAndUpdate(
+      id , 
+      data , 
+      {
+        new : true
+      }
+    )
   }
 
-  async delete(id) {
-    return await Product.findByIdAndDelete(id);
+  buildQuery(filters) {
+    let query = {};
+
+    if (filters.search) {
+      // Case-insensitive search
+      query.name = { $regex: filters.search, $options: 'i' }; 
+    }
+
+    if (filters.minPrice) {
+      query.price = { $gte: filters.minPrice };
+    }
+
+    if (filters.maxPrice) {
+      query.price = query.price || {}; 
+      query.price.$lte = filters.maxPrice;
+    }
+
+    if (filters.status) {
+      query.status = filters.status;
+    }
+
+    if (filters.includeDeleted) {
+      // Including soft-deleted products
+      query.deletedAt = { $ne: null }; 
+    } else {
+      // Excluding soft-deleted products
+      query.deletedAt = null; 
+    }
+
+    return query;
   }
 }
 
-export default new ProductRepository();
+export default ProductRepository;
