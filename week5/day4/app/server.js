e7be679ceceb8e6a79b3e64cd3f8cfa98292adc8
ansh-stack00 <ansha.hestabit@gmail.com>
2025-12-09 17:52:40 +0530

    completed day 4

---
+++ week5/day4/app/server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("HTTPS working with mkcert + NGINX!\n");
+});
+
+server.listen(3000, () => {
+  console.log("App running on port 3000");
+});
+  
