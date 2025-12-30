bcbea7063f67543bfdc4e445c73fd58756334f86
ansh-stack00 <ansha.hestabit@gmail.com>
2025-12-30 12:20:36 +0530

    feat: created a RAG pipeline to talk with files

---
+++ week7-GenAI/src/generator/llm_client.py
@@ -0,0 +1,7 @@
+from openai import OpenAI
+
+def get_llm():
+    return  OpenAI(
+        base_url="https://api.groq.com/openai/v1",
+        api_key="REMOVED"
+    )
---
+++ week7-GenAI/src/retriever/query_engine.py
@@ -0,0 +1,57 @@
+from src.embeddings.embedder import get_embedding_model
+from src.generator.llm_client import get_llm
+from dotenv import load_dotenv
+from langchain_qdrant import QdrantVectorStore
+
+
+load_dotenv()
+
+embedding_model = get_embedding_model()
+
+client = get_llm()
+
+vector_db = QdrantVectorStore.from_existing_collection(
+    url="http://localhost:6333",
+    collection_name="enterprise_docs",
+    embedding=embedding_model,
+
+
+)
+
+# take user  input
+
+user_query = input("Ask something:")
+
+search_results = vector_db.similarity_search(query=user_query)
+
+
+context ="\n\n\n".join([f"Page Content:{result.page_content}\nPage Number:{result.metadata['page_label']}\nFile Loaction:{result.metadata['source']}" for result in search_results])
+
+SYSTEM_PROMPT = f"""
+You are  helpful AI Assistant who answers user query based on the available context
+retrieved from a PDF file along with page_contents and page number.
+
+You should only answer the user based on the following context and navigate the user to 
+open the right page number to know more .
+
+context:
+{context}
+
+"""
+
+
+responses = client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[
+        {
+            "role":"system",
+            "content":SYSTEM_PROMPT
+        },
+        {
+            "role":"user",
+            "content":user_query
+        }
+    ]
+)
+
+print(f"🤖 {responses.choices[0].message.content}")
