da29fe2a63054b951ed9e01e86787b1a91deab9b
ansh-stack00 <ansha.hestabit@gmail.com>
2026-01-17 22:07:53 +0530

    data preprocessing done

--- week6/src/pipelines/data_pipeline.py
+++ week6/src/pipelines/data_pipeline.py
@@ -5,45 +5,73 @@ import os
 SRC_FILE_PATH = os.path.join(os.getcwd(), 'src/data/raw/train.csv')
 DEST_FILE_PATH = os.path.join(os.getcwd(), 'src/data/processed/final.csv')
 
-def load_data():
-    return pd.read_csv(SRC_FILE_PATH)
 
-def clean_data(data):
-    if "Cabin" in data.columns:
-        data = data.drop(columns=["Cabin"])
+def load_Data():
+    df=pd.read_csv(SRC_FILE_PATH)
+    print(f"data loaded succesfully : {df.shape}")
+    # print(df.head())
 
-    numeric_cols = data.select_dtypes(include=[np.number]).columns
-    data[numeric_cols] = data[numeric_cols].fillna(data[numeric_cols].median())
+    return df
 
-    categorical_cols = data.select_dtypes(include=['object']).columns
-    for col in categorical_cols:
-        data[col] = data[col].fillna(data[col].mode()[0])
+def handling_duplicated_values(df):
 
-    data = data.drop_duplicates()
+    # checking duplicated values 
+    # print(df.duplicated())
+    duplicate_values = df.duplicated().sum()
 
-    def remove_outliers_iqr(data, cols):
-        for col in cols:
-            Q1 = data[col].quantile(0.25)
-            Q3 = data[col].quantile(0.75)
-            IQR = Q3 - Q1
+    if duplicate_values > 0:
+        print(df[df.duplicated()])
+        df = df.drop_duplicates(inplace=True)
+        print(f"removed dupicate valeus")
+    else :
+        print("no duplicate values found ")
+    
 
-            lower_bound = Q1 - 1.5 * IQR
-            upper_bound = Q3 + 1.5 * IQR
+    return df
 
-            data = data[(data[col] >= lower_bound) & (data[col] <= upper_bound)]
-        return data
+def handling_missing_values(df):
+    # show_missed_values = df.isnull().sum()
+    # print(show_missed_values)
 
-    data = remove_outliers_iqr(data, ['Age', 'Fare'])
-    return data
+    df["Age"] = df["Age"].fillna(df["Age"].median())
+    df["Embarked"] = df["Embarked"].fillna(df["Embarked"].mode()[0])
 
-def save_cleaned_data(data):
-    data.to_csv(DEST_FILE_PATH, index=False)
+    # 687 missing values 
+    df = df.drop(columns=["Cabin"])
+    print(f"after handling missing values  {df.shape}")
 
-def main():
-    data = load_data()
-    cleaned_data = clean_data(data)
-    save_cleaned_data(cleaned_data)
-    print("Data processing complete and saved to src/data/processed/final.csv")
+    return df
+
+
+def handle_outliers(df):
+    numerical_cols = ["Age", "Fare"]
+    for col in numerical_cols:
+
+        Q1 = df[col].quantile(0.25)
+        Q3 = df[col].quantile(0.75)
+        IQR = Q3 - Q1
+        lower = Q1 - 1.5 * IQR
+        upper = Q3 + 1.5 * IQR
+        df = df[(df[col] >= lower) & (df[col] <= upper)]
+
+    return df
+
+
+def save_processed_data(df):
+    df.to_csv(DEST_FILE_PATH, index=False)
+    print(f"Processed data saved at {DEST_FILE_PATH}")
+
+    
+
+
+def run_pipeline():
+    df = load_Data()
+    df = handling_duplicated_values(df)
+    df = handling_missing_values(df)
+    df = handle_outliers(df)
+    save_processed_data(df)
+
 
 if __name__ == "__main__":
-    main()
+    run_pipeline()
+    
