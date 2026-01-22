6c7044211849747d4e8a573bc67c74267341accc
ansh-stack00 <ansha.hestabit@gmail.com>
2026-01-22 15:57:54 +0530

    DAG execution

--- week9/day2/utils/dag.py
+++ week9/day2/utils/dag.py
@@ -1,11 +1,8 @@
-# utils/dag.py
 from typing import Dict
 from utils.models import TaskStatus, PlanModel
 
-
 def init_task_status(plan: PlanModel) -> Dict[str, TaskStatus]:
     return {t.id: TaskStatus.PENDING for t in plan.tasks}
-
 
 def get_ready_tasks(plan: PlanModel, task_status: Dict[str, TaskStatus]):
     return [
