858dd49bb753e18c267cef23c867af2d29e07818
ansh-stack00 <ansha.hestabit@gmail.com>
2026-02-05 13:15:16 +0000

    chor : fix the paths

--- week9/nexusAi_day5/config.py
+++ week9/nexusAi_day5/config.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 BASE_DIR = Path(".")
 
-OUTPUT_DIR = BASE_DIR / "nexus_output"
-LOG_DIR = BASE_DIR / "logs"
+OUTPUT_DIR = BASE_DIR / "nexusAi_day5" / "nexus_output"
+LOG_DIR = BASE_DIR /"nexusAi_day5" / "logs"
 
 OUTPUT_DIR.mkdir(exist_ok=True)
 LOG_DIR.mkdir(exist_ok=True)
